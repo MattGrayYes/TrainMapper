33706ba4ff0beb95c5617d6ce94424d2edb01699
Matt Gray <804029+MattGrayYes@users.noreply.github.com>
2024-12-15 17:49:19 +0000

    added web server version for dynamically entering train details

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ This is still a first proof of concept that I'm playing with. Don't expect it to
     * For example: Kings Cross' lat/lon is directly above the Piccadilly line. So it routes you out of KGX along the Piccadilly Line to Gunnersbury, along the Suffragette Line, and then onto the ECML.
 
 ## Use
+### Requirements
 * Requires [Deno](https://deno.com/)
 * Requires [RealTimeTrains API](https://api.rtt.io/) key.
 
+### Running It
+1. Run `deno task web`
+1. Go to http://localhost:8080/
+
+### CLI version
 1. Change `serviceUid` and `serviceDate` in main.ts to match your train.
     * Search for a train on https://www.realtimetrains.co.uk, and copy the UID from the URL.
 1. Run `deno task dev`
@@ -21,7 +27,7 @@ This is still a first proof of concept that I'm playing with. Don't expect it to
 * C47426, 2024/12/14: [1000 Kings Cross to Aberdeen on RealTimeTrains](https://www.realtimetrains.co.uk/service/gb-nr:C47426/2024-12-14#allox_id=0)
 * [Generated map of route on signal.eu.org](https://signal.eu.org/osm/#locs=51.532395,-0.123022;53.957704,-1.093730;54.520662,-1.546691;54.968336,-1.616046;55.774555,-2.010542;55.951902,-3.190420;55.945184,-3.219374;56.035239,-3.395417;56.111999,-3.167054;56.374796,-2.893855;56.457149,-2.969749;56.559338,-2.589274;56.712830,-2.472217;56.966862,-2.225283;57.142649,-2.097635)
 
-### Output
+### CLI Output
 
 ```
 deno task dev
@@ -51,6 +57,4 @@ STN STN Stonehaven
 ABD ABD Aberdeen
 
  https://signal.eu.org/osm/#locs=51.5323954,-0.1230224;53.9577037,-1.0937301;54.5206617,-1.5466911;54.9683364,-1.616046;55.7745555,-2.0105423;55.9519018,-3.1904199;55.9451838,-3.2193738;56.0352386,-3.3954171;56.1119986,-3.1670545;56.374796,-2.8938545;56.4571485,-2.9697488;56.5593376,-2.5892737;56.7128296,-2.4722165;56.9668616,-2.225283;57.1426487,-2.0976346;
-
-**
 ```
---
+++ web.ts
@@ -0,0 +1,141 @@
+import "jsr:@std/dotenv/load";
+// Configure your .env file with your RTT API key details.
+// Select your train here
+
+const rttApiKey: string = Deno.env.get("RTTAPIKEY") as string;
+const rttApiSecret: string = Deno.env.get("RTTAPISECRET") as string;
+
+import { Application, Router } from "jsr:@oak/oak";
+
+console.log("\nTrainMapper Web started\n\n\n")
+
+const router = new Router();
+
+router.get("/", async (ctx) => {
+    ctx.response.body = 
+       `<!doctype html>
+        <html>
+        <head>
+            <title>Train Mapper</title>
+        </head>
+        <body>
+            <h1>Train Mapper</h1>
+            <p>To use this:</p>
+            <ol>
+                <li>Find a train using <a href="https://www.realtimetrains.co.uk/">RealTimeTrains</a> or similar
+                <li>Look for the service UID and date of the train you want to map. You can get these from the end of a RealTimeTrains service URL (<a href="https://www.realtimetrains.co.uk/service/gb-nr:C71480/2024-12-15">for example this one</a>)
+                <li>Append them to the URL of this page in the format /serviceUid/yyy/mm/dd (for example <a href="/C47426/2024/12/14">C47426/2024/12/14</a>)
+            </ol>
+            <hr>
+            <p><a href="https://mattg.co.uk">Matt Gray</a></p>
+        </body>
+        `;
+});
+
+router.get("/:serviceUid/:yyyy/:mm/:dd", async (ctx) => {
+    await getRoute(ctx.params.serviceUid, ctx.params.yyyy, ctx.params.mm, ctx.params.dd)
+        .then((mapUrl) => {
+            console.log("Got MapUrl", mapUrl)
+            ctx.response.body = "<html><a href=\"" + mapUrl + "\">" + mapUrl + "</a>";
+        })
+        .catch((err) => {
+            console.error("getroute caught Error: ", err);
+            ctx.response.body = "Booo there's been an error :(\n"+err;
+        });
+});
+
+
+
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
+
+async function getRoute(serviceUid: string, yyyy: string, mm: string, dd: string) {
+    console.log("\n**Train Mapper**\n");
+
+    let mapurl: string = "https://signal.eu.org/osm/#locs=";
+    const crses: string[] = [];
+    let query: string = "[out:json];\n(\n";
+    const queryEnd: string = ");\nout geom;";
+    const locations: string[] = [];
+
+    const dateString: string = yyyy + "/" + mm + "/" + dd;
+
+    console.log("http://api.rtt.io/api/v1/json/service/" + serviceUid + "/" + dateString)
+    await fetch(
+        "http://api.rtt.io/api/v1/json/service/" + serviceUid + "/" + dateString,
+        {
+            method: "POST",
+            headers: {
+                "Authorization": "Basic " + btoa(rttApiKey + ":" + rttApiSecret),
+            },
+        })
+        .then(async (response) => {
+            console.log(response);
+
+            if(response.status != 200)
+            {
+                throw new Error("RTT API HTTP status "+response.status+" "+response.statusText)
+            }
+            else{
+                const json = await response.json()
+                .catch((err) => {
+                    console.error(err, response, response.body);
+                    throw new Error("RTT API response JSON parsing error "+ response)
+                });
+              
+                if (json.hasOwnProperty("error")) {
+                    throw new Error("RTT API Response error: " + json.error)
+                }
+                else
+                    return json;
+            }
+        })
+        .then((train) => {
+            console.log(train);
+            console.log(
+                train.origin[0].publicTime,
+                train.origin[0].description,
+                "to",
+                train.destination[0].description,
+            );
+            console.log(
+                train.serviceUid,
+                train.trainIdentity,
+                train.atocName,
+                "\n\nCalling at:",
+            );
+            train.locations.forEach((location) => {
+                console.log(location.crs, location.crs, location.description);
+                crses.push(location.crs);
+            });
+        })
+        .then(async () => {
+            crses.forEach((crs: string) => {
+                query += 'nwr["ref:crs"="' + crs + '"];\n';
+            });
+            query += queryEnd;
+            return await fetch("https://overpass-api.de/api/interpreter", {
+                method: "POST",
+                body: query,
+            });
+        })
+        .then((response) => response.json())
+        .then((locData) => {
+            locData.elements.forEach((loc) => {
+                locations[loc.tags["ref:crs"]] = loc.lat + "," + loc.lon;
+            });
+
+            crses.forEach((crs: string) => {
+                mapurl += locations[crs] + ";";
+            });
+
+            console.log("\n", mapurl);
+            console.log("\n**\n");
+        });
+
+    return mapurl;
+}
